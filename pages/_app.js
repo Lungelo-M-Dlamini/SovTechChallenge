e8b9f3bfd18a4a153614406346fb528d289ce624
Lungelo-M-Dlamini <69636868+Lungelo-M-Dlamini@users.noreply.github.com>
2021-12-10 17:57:33 +0200

    Image props for skills

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { Component } from 'react/cjs/react.production.min'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+MyApp.getInitialProps = async ({Comment, ctx}) =>{
+  const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+  if(Object.keys(pageProps).length >0){
+    return(pageProps);
+  }
+  else{
+    return {};
+  }
+};
 
 export default MyApp
